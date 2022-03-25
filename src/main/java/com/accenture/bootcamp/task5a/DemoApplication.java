60775199f89eb888473a68a8ed1db4f7e545c421
AleksandrsC <mirrorfrequency@gmail.com>
2022-03-25 14:15:58 +0200

    Task 2

--- src/main/java/com/accenture/bootcamp/task5a/DemoApplication.java
+++ src/main/java/com/accenture/bootcamp/task5a/DemoApplication.java
@@ -2,11 +2,12 @@ package com.accenture.bootcamp.task5a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.accenture.bootcamp"})
 @RestController
 public class DemoApplication {
 
@@ -14,6 +15,11 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	/**
+	 * Hello world (or hello user) in plain text over HTTP
+	 * @param name name of the user to greet, "world" by default
+	 * @return
+	 */
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
